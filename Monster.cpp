2b0f0734f0ac747f38d7e4e204ef698b9776c061
Quads45 <samedprice@gmail.com>
2017-03-07 19:21:01 +0000

    Created Monster Class
    we cna more shit
    still need finishing touches

--- Monster.cpp
+++ Monster.cpp
@@ -1,1 +1,73 @@
 #include "Monster.h"
+#include "libsqlite.hpp"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+Monster::Monster(int MonsterLevelSelect){
+
+	
+	if (MonsterLevelSelect%5!=0){
+		sqlite::sqlite db("dung.db");
+
+		auto cur = db.get_statement();
+
+		cur->set_sql("SELECT COUNT(*) FROM Monster WHERE MonsterLevel = ?;");
+		cur->prepare();
+		cur->bind(1, MonsterLevelSelect);
+		cur->step();
+
+		MonsterSelect = cur->get_int(0);
+
+		MonsterID = RNG(MonsterSelect);
+		
+	}
+	else {
+		sqlite::sqlite db("dung.db");
+
+		auto cur = db.get_statement();
+
+		cur->set_sql("SELECT MonsterID FROM Monster WHERE MonsterLevel = ?;");
+		cur->prepare();
+		cur->bind(1, MonsterLevel);
+		cur->step();
+		MonsterID = cur->get_int(0);
+	}
+	sqlite::sqlite db("dung.db");
+
+	auto cur = db.get_statement();
+
+	cur->set_sql("SELECT [MonsterName],[MonsterHP],[MonsterDefence],[MonsterAttackDamage],[MonsterDodge],[MonsterAccuracy],[MonsterExpValue],[MonsterAttackSpeed],[MonsterLevel] FROM [Monster] WHERE [MonsterID]=?;");
+	cur->prepare();
+	cur->bind(1, MonsterID);
+	cur->step();
+
+	MonsterName = cur->get_text(0);
+	MonsterHP = cur->get_int(1);
+	MonsterDefence = cur->get_int(2);
+	MonsterAttackDamage = cur->get_int(3);
+	MonsterDodge = cur->get_int(4);
+	MonsterAccuracy = cur->get_int(5);
+	MonsterExpValue = cur->get_int(6);
+	MonsterAttackSpeed = cur->get_double(7);
+	MonsterLevel = cur->get_int(8);
+}
+
+// Random Time Generator.cpp : Defines the entry point for the console application.
+//
+string Monster::getMonsterName() {
+	return MonsterName;
+}
+
+
+int RNG(int maxValue)
+{
+	int count;
+	int RandomNumber;
+	
+	srand(time(0));
+	
+	return rand() % (maxValue + 1);
+}
--- Monster.h
+++ Monster.h
@@ -1,8 +1,26 @@
 #ifndef  Monster_H
 #define Monster_H
+#include <string>
 
 class Monster {
-	int MonsterHP, MonsterDefence, 
+	int MonsterID, MonsterHP, MonsterSelect, MonsterDefence, MonsterAttackDamage, MonsterDodge, MonsterAccuracy, MonsterExpValue, MonsterLevel;
+	std::string MonsterName;
+	double MonsterAttackSpeed;
+	bool MonsterBoss;
+
+public:
+	Monster(int MonsterLevelSelect);
+	int getMonsterAD();
+	int getMonsterHP();
+	double getMonsterAS();
+	int getMonsterDodge();
+	int getMonsterDefence();
+	std::string getMonsterName();
+	int getMonsterAccuracy();
+	bool getMonsterBoss();
+	int getMonsterExpValue();
+	int getMonsterLevel();
+
 };
 
 #endif 
